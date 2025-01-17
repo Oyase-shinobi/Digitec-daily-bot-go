dd33cd1695e0862c5541b7309e16f39d6d31b603
Oyase-shinobi <donjosy305@gmail.com>
2025-01-17 22:35:15 +0900

    update addDefaulthandler funtionality

--- types/task.go
+++ types/task.go
@@ -7,3 +7,13 @@ type TaskSettings struct {
 	Url                 string  `bson:"url"`
 	CurrentTextTemplate string  `bson:"current_text"`
 }
+
+func addDefaultHandler(b *tb.Bot) {
+	logger.Println("add default handler")
+	b.Handle(tb.OnText, func(c tb.Context) error {
+		logger.Println("handle text")
+		logger.Println(c.Text())
+		return c.Send("unknown command: " + c.Text())
+	})
+
+}
